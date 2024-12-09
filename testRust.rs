a8bc2bca5ea46817771b3bd202963812184676a5
Melanie Dohr <114560271+DohrMelanie@users.noreply.github.com>
2024-12-09 10:12:13 +0100

    Add files via upload

---
+++ testRust.rs
@@ -0,0 +1,31 @@
+extern crate colored; 
+use colored::*; 
+fn main() {
+    println!( 
+		"{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}",
+		("Oh, the weather outside is frightful").white(),
+		("\nBut the fire is so delightful").red(),
+		("\nAnd since we've no place to go").white(), 
+		("\nLet it snow Let it snow Let it snow").yellow(),
+		("\nMan it doesn't show signs of stopping").green(),
+		("\nAnd I brought me some corn for popping").blue(),
+		("\nThe lights are turned way down low").purple(),
+		("\nLet it snow Let it snow").cyan(),
+		("\nWhen we finally kiss goodnight").white(),
+		("\nHow I'll hate going out in the storm").red(),
+		("\nBut if you'll really hold me tight").white(),
+		("\nAll the way home I'll be warm").yellow(),
+		("\nAnd the fire is slowly dying").green(),
+		("\nAnd, my dear, we're still goodbying").blue(),
+		("\nBut as long as you'd love me so").purple(),
+		("\nLet it snow Let it snow and snow").cyan(),
+		("\nWhen we finally kiss goodnight").white(),
+		("\nHow I'll hate going out in the storm").red(),
+		("\nBut if you really grab me tight").white(),
+		("\nAll the way home I'll be warm").yellow(),
+		("\nOh, the fire is slowly dying").green(),
+		("\nAnd, my dear, we're still goodbying").blue(),
+		("\nBut as long as you'd love me so").purple(),
+		("\nLet it snow, let it snow, let it snow").cyan()
+	);
+}
